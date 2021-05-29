304b7c5e337d04a1024aa223f333c7bc912b5e14
Rodrigo Cunha <rcunha@wkm.com.br>
2021-05-29 16:03:27 -0300

    Criar modelo Category

---
+++ app/Models/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FinancialControl\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'type',
+    ];
+}
