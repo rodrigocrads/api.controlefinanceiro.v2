d742594c718f06292750131bf982c05a424d67f2
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-06-12 13:49:14 -0300

    Utilizar classe DTO FixedRevenueResponse no retorno das actions ListAll e GetById

--- app/Actions/FixedRevenue/GetById.php
+++ app/Actions/FixedRevenue/GetById.php
@@ -4,11 +4,18 @@ namespace FinancialControl\Actions\FixedRevenue;
 
 use FinancialControl\Models\FixedRevenue;
 use FinancialControl\Actions\AbstractAction;
+use FinancialControl\Custom\DTO\FixedRevenueResponse;
+use FinancialControl\Exceptions\NotFoundException;
 
 class GetById extends AbstractAction
 {
     public function run()
     {
-        return FixedRevenue::find($this->data['id']);
+        $fixedRevenue = FixedRevenue::find($this->data['id']);
+
+        if ($fixedRevenue === null) throw new NotFoundException('Fixed revenue not found.');
+
+        return (new FixedRevenueResponse($fixedRevenue))
+            ->toArray();
     }
 }
