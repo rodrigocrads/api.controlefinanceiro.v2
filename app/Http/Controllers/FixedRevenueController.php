578e4b7f293b5b7d81ad864c76b0b736cf8761af
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-06-12 10:54:51 -0300

    Refatorar metodo save em FixedRevenueController

--- app/Http/Controllers/FixedRevenueController.php
+++ app/Http/Controllers/FixedRevenueController.php
@@ -11,19 +11,12 @@ class FixedRevenueController extends Controller
     public function save(SaveRequest $request)
     {
         try {
-            $activationControlData = $request->get('activation_control');
-
             $action = resolve(Save::class, [
                 'data' => [
                     'title' => $request->get('title'),
                     'description' => $request->get('description'),
                     'value' => $request->get('value'),
-                    'activation_control' => [
-                        'start_date' => data_get($activationControlData, 'start_date'),
-                        'end_date' => data_get($activationControlData, 'end_date'),
-                        'activation_day' => data_get($activationControlData, 'activation_day'),
-                        'activation_type' => data_get($activationControlData, 'activation_type'),
-                    ]
+                    'activation_control' => $request->get('activation_control')
                 ]
             ]);
 
