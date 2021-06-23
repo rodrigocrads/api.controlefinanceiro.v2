6a808e4d0cbe776d471b67b184dd9cb3e7c820ed
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-06-22 21:10:23 -0300

    Adicionar dado register_date no retorno do dto response

--- app/Custom/DTO/VariableExpenseOrRevenue.php
+++ app/Custom/DTO/VariableExpenseOrRevenue.php
@@ -20,6 +20,7 @@ class VariableExpenseOrRevenue
             'title' => $this->model->title,
             'description' => $this->model->description,
             'value' => (double) $this->model->value,
+            'register_date' => $this->model->register_date,
             'category' => [
                 'id' => $this->model->category->id,
                 'name' => $this->model->category->name,
