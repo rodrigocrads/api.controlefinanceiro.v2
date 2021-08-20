5ec0f4dac9d87525784d79afb3fd4f43d2f3ae1a
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-08-19 21:21:28 -0300

    feat: utilizar laravel auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/login', 'Auth\LoginController@show')->name('login');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
