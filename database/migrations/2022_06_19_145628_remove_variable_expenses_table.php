2a3572b376d8c80d33190e15a919334c7b1fce6a
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2022-01-01 14:29:41 -0300

    chore(Remove/Tables/VariableExpense/VariableRevenue)

---
+++ database/migrations/2022_06_19_145628_remove_variable_expenses_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveVariableExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('variable_expenses');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('variable_expenses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('title', 100);
+            $table->string('description', 255)->nullable();
+            $table->double('value');
+            $table->date('register_date');
+
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories');
+            
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+}
